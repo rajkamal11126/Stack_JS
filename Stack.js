dd6a4aa0de6dac0ead155d864782f7d37d1e6e68
Raj <rajkamal11126@gmail.com>
2020-11-21 02:22:32 +0530

    Added : Functionality to display the element in the stack

--- Stack.js
+++ Stack.js
@@ -1,8 +1,25 @@
 const Utility = require('./Utility');
 let readlineSync = require('readline-sync');
-/**
- * take input from the user and push the element
- */
-let element = readlineSync.question("Enter push Integer:");
-Utility.push(element);
-Utility.pop();
+
+/** method for choose the option for push, pop and display the element */
+choice = function () {
+    let isTerminated = true;
+    while (isTerminated == true) {
+        /** take choice input from the user */
+        let choice = readlineSync.question("\nEner your choice:\n1.push\n2.pop\n3.print\n4.exit\n");
+        switch (choice) {
+            case '1': let element = readlineSync.question("Enter the element:");
+                Utility.push(element);
+                break;
+            case '2': Utility.pop();
+                break;
+            case '3': Utility.print();
+                break;    
+            case '4': isTerminated = false;
+                break;
+            default: console.log("please enter correct option");
+        }
+    }
+}
+choice();
+
--- Utility.js
+++ Utility.js
@@ -3,18 +3,28 @@ class Stack {
         this.data = [];
         this.top = 0;
     }
-    /**method for push the element */
+    /** method for push the element */
     push = (element) => {
         this.data[this.top] = element;
         this.top = this.top + 1;
     }
+    /** method for check stack is empty or not */
     isEmpty = () => {
         return this.top === 0;
     }
-    pop() {
+    /** method for pop the element  */
+    pop = () => {
         if (this.isEmpty() === false) {
             this.top = this.top - 1;
             return this.data.pop(); // removes the last element
+        }
+    }
+    /**method for display the element */
+    print = () => {
+        let top = this.top - 1; // because top points to index where new element to be inserted
+        while (top >= 0) { // print upto 0th index
+            console.log(this.data[top]);
+            top--;
         }
     }
 }
